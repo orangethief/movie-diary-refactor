c133e88d2fe2663d7936661b52559b64304f8050
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-08-03 12:01:36 +0200

    Fixed hiding cards when searching in diary

--- src/shared.js
+++ src/shared.js
@@ -11,14 +11,14 @@ export function updateMovie(movie) {
     journal[index] = movie;
     updateJournal(journal);
   }
-  
+
   export function addToJournal(movie) {
     const journal = getJournal();
     if (isInJournal(movie)) return;
     journal.push({ ...movie, notes: [] });
     updateJournal(journal);
   }
-  
+
   export function removeFromJournal(movie) {
     const journal = getJournal();
     if (!isInJournal(movie)) return;
@@ -26,19 +26,19 @@ export function updateMovie(movie) {
     journal.splice(index, 1);
     updateJournal(journal);
   }
-  
+
   export function isInJournal(movie) {
     return getJournal().some(entry => movie.id === entry.id);
   }
-  
+
   export function getJournal() {
     return JSON.parse(localStorage.getItem('journal') || '[]');
   }
-  
+
   export function updateJournal(journal) {
     localStorage.setItem('journal', JSON.stringify(journal));
   }
-  
+
   // note management functions
   export function showNotes(movie) {
     const modal = document.getElementById('modal');
@@ -46,23 +46,23 @@ export function updateMovie(movie) {
     const notesList = document.getElementById('movieNotes');
     const addNoteInput = document.getElementById('new-note');
     const addNoteBtn = document.getElementById('addNote');
-  
+
     // update modal styling
     modal.classList.add('bg-gray-800', 'text-white', 'rounded-lg', 'shadow-xl', 'p-6', 'max-w-2xl', 'w-full');
-  
+
     // style the title
     title.className = 'text-3xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600';
     title.innerText = movie.original_title;
-  
+
     // style the notes list
     notesList.className = 'space-y-2 mb-4';
     notesList.innerHTML = '';
     movie.notes.forEach(note => notesList.appendChild(createNote(note, movie)));
-  
+
     // style the input and button
     addNoteInput.className = 'w-full bg-gray-700 text-white px-4 py-2 rounded-lg mb-2';
     addNoteBtn.className = 'w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white font-bold py-2 px-4 rounded-lg hover:from-purple-600 hover:to-pink-600 transition duration-300';
-  
+
     addNoteInput.value = '';
     addNoteBtn.onclick = () => {
         const note = addNoteInput.value.trim();
@@ -73,11 +73,11 @@ export function updateMovie(movie) {
         notesList.appendChild(createNote(newNote, movie));
         addNoteInput.value = '';
     };
-  
+
     modal.classList.remove("closing");
     modal.showModal();
     modal.classList.add("showing");
-  
+
     const closeModal = () => {
         modal.classList.remove("showing");
         modal.classList.add("closing");
@@ -86,32 +86,32 @@ export function updateMovie(movie) {
             modal.classList.remove("closing");
         }, { once: true });
     };
-  
+
     const closeModalTop = document.getElementById('closeModalTop');
     const closeModalBottom = document.getElementById('closeModalBottom');
-  
+
     closeModalTop.className = 'absolute top-2 right-2 text-gray-500 hover:text-white transition duration-300';
     closeModalBottom.className = 'mt-4 bg-gray-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-300';
-  
+
     closeModalTop.onclick = closeModal;
     closeModalBottom.onclick = closeModal;
   }
-  
+
   export function createNote(note, movie) {
     const noteEntry = document.createElement('div');
     noteEntry.className = 'bg-gray-700 rounded-lg p-3 flex justify-between items-center';
-  
+
     const noteText = document.createElement('p');
     noteText.className = 'text-white';
     noteText.innerHTML = note.note;
-  
+
     dayjs.extend(relativeTime);
     const createdAt = dayjs(note.created_at);
     const createdAtEl = document.createElement('cite');
     createdAtEl.innerText = createdAt.fromNow();
     createdAtEl.classList.add('block', 'text-sm', 'text-gray-500', 'dark:text-gray-400');
     noteText.appendChild(createdAtEl);
-  
+
     const removeNoteBtn = document.createElement('button');
     removeNoteBtn.type = 'button';
     removeNoteBtn.className = 'text-red-500 hover:text-red-700 transition duration-300';
@@ -122,12 +122,12 @@ export function updateMovie(movie) {
         noteEntry.remove();
         updateMovie(movie);
     });
-  
+
     noteEntry.appendChild(noteText);
     noteEntry.appendChild(removeNoteBtn);
     return noteEntry;
   }
-  
+
   // movie genres
   export const genres = [
     { id: 28, name: "Action" }, { id: 12, name: "Adventure" }, { id: 16, name: "Animation" },
@@ -138,43 +138,43 @@ export function updateMovie(movie) {
     { id: 10770, name: "TV Movie" }, { id: 53, name: "Thriller" }, { id: 10752, name: "War" },
     { id: 37, name: "Western" }
   ];
-  
+
   // movie card creation
   export const createMovieCard = (movie, isDiary = false) => {
     const templateDiv = document.createElement('div');
-    templateDiv.classList.add('bg-gray-800', 'rounded-lg', 'overflow-hidden', 'shadow-lg', 'movie-card', 'relative');
-  
+    templateDiv.classList.add('bg-gray-800', 'rounded-lg', 'overflow-hidden', 'shadow-lg', 'flex', 'movie-card', 'relative');
+
     const poster = document.createElement('img');
     poster.src = movie.poster_path
         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : "https://i.pinimg.com/originals/cd/23/c7/cd23c7a8d049049fd1b0ef281f0300cb.jpg";
     poster.classList.add('movie-poster', 'hover:opacity-65', 'transition', 'duration-300');
     poster.alt = movie.original_title;
-  
+
     const contentDiv = document.createElement('div');
     contentDiv.classList.add('p-4');
-  
+
     const movieTitle = document.createElement('h3');
     movieTitle.textContent = movie.original_title.toUpperCase();
     movieTitle.classList.add('text-xl', 'font-semibold', 'mb-2', 'mt-1', 'ml-1', 'text-center', 'truncate');
-  
+
     const descriptionP = document.createElement('p');
     descriptionP.classList.add('text-gray-400', 'mb-4', 'text-center');
     const genre = genres.find(x => x.id === movie.genre_ids[0]);
     descriptionP.textContent = `${movie.release_date ? movie.release_date.slice(0, 4) : 'Unknown'} | ${genre?.name || 'Unknown'}`;
-  
+
     // create a container for buttons
     const buttonContainer = document.createElement('div');
     buttonContainer.classList.add('flex', 'flex-col', 'space-y-2'); // Add vertical spacing between buttons
-  
+
     templateDiv.append(poster, contentDiv);
     contentDiv.append(movieTitle, descriptionP, buttonContainer);
-  
+
     if (isInJournal(movie)) {
         if (isDiary) {
             const showNotesBtn = createButton('Notes', 'fa-note-sticky', () => showNotes(movie));
-           
-          
+
+
             buttonContainer.append(showNotesBtn);
         }
     } else {
@@ -182,10 +182,10 @@ export function updateMovie(movie) {
             addToJournal(movie);
             addToDiaryBtn.remove(); */
         //});
-            
+
     }
 
-             const addToDiaryBtnSpan = document.createElement('span');        
+             const addToDiaryBtnSpan = document.createElement('span');
             const addToDiaryIcon = document.createElement('i');
             addToDiaryIcon.classList.add('fa-regular', 'fa-heart', 'text-4xl');
             addToDiaryBtnSpan.classList.add('cursor-pointer', 'absolute', 'top-3', 'right-3');
@@ -199,12 +199,12 @@ export function updateMovie(movie) {
             if (isInJournal(movie)) {
               addToDiaryBtnSpan.classList.add('hidden');}
 
-              const removeFromDiaryBtnSpan = document.createElement('span'); 
+              const removeFromDiaryBtnSpan = document.createElement('span');
               const removeFromDiaryIcon = document.createElement('i');
               removeFromDiaryBtnSpan.classList.add('cursor-pointer', 'absolute', 'top-3', 'right-3');
             removeFromDiaryIcon.classList.add('fa-solid', 'fa-heart', 'text-4xl');
             removeFromDiaryBtnSpan.addEventListener('click', () => {
-              
+
                 removeFromJournal(movie);
                 removeFromDiaryBtnSpan.classList.add('hidden');
                 addToDiaryBtnSpan.classList.remove('hidden');
@@ -220,12 +220,12 @@ export function updateMovie(movie) {
        removeFromDiaryBtnSpan.appendChild(removeFromDiaryIcon);
         templateDiv.appendChild(addToDiaryBtnSpan);
         templateDiv.appendChild(removeFromDiaryBtnSpan);
-  
+
     return templateDiv;
   };
-  
 
-  
+
+
   function createButton(text, iconClass, onClick) {
     const button = document.createElement('button');
     button.classList.add('gradient-border', 'w-full', 'mt-2'); // Added mt-2 for top margin
@@ -236,7 +236,7 @@ export function updateMovie(movie) {
     button.addEventListener('click', onClick);
     return button;
   }
-  
+
   // utility function to debounce the search input
   export const debounce = (func, wait) => {
       let timeout;
@@ -245,4 +245,3 @@ export function updateMovie(movie) {
           timeout = setTimeout(() => func.apply(this, args), wait);
       };
   }
-  
